4aeebcfeaf68ff666462445df9a6671cdcb46c39
fazliddin <hajrullaevfazliddin058@gmail.com>
2024-09-23 16:10:04 +0500

    fixsed

---
+++ migrations/000002_auth_service_table.up.sql
@@ -0,0 +1,22 @@
+INSERT INTO countries (country, nationality, flag)
+VALUES
+    ('Turkey', 'Turkish', 'https://flagpedia.net/data/flags/h80/tr.png'),
+    ('Azerbaijan', 'Azerbaijani', 'https://flagpedia.net/data/flags/h80/az.png'),
+    ('Kazakhstan', 'Kazakh', 'https://flagpedia.net/data/flags/h80/kz.png'),
+    ('Uzbekistan', 'Uzbek', 'https://flagpedia.net/data/flags/h80/uz.png'),
+    ('Turkmenistan', 'Turkmen', 'https://flagpedia.net/data/flags/h80/tm.png'),
+    ('Kyrgyzstan', 'Kyrgyz', 'https://flagpedia.net/data/flags/h80/kg.png'),
+    ('Tatarstan', 'Tatar', 'https://flagpedia.net/data/flags/h80/ru.png'), -- Автономия в России
+    ('Bashkortostan', 'Bashkir', 'https://flagpedia.net/data/flags/h80/ru.png'), -- Автономия в России
+    ('Chuvashia', 'Chuvash', 'https://flagpedia.net/data/flags/h80/ru.png'), -- Автономия в России
+    ('Altai', 'Altai', 'https://flagpedia.net/data/flags/h80/ru.png'), -- Автономия в России
+    ('Tuva', 'Tuvan', 'https://flagpedia.net/data/flags/h80/ru.png'), -- Автономия в России
+    ('Khakassia', 'Khakas', 'https://flagpedia.net/data/flags/h80/ru.png'), -- Автономия в России
+    ('Crimea', 'Crimean Tatar', 'https://flagpedia.net/data/flags/h80/ru.png'), -- Автономия в России
+    ('China', 'Uyghur', 'https://flagpedia.net/data/flags/h80/cn.png'), -- Флаг Китая
+    ('Gagauzia', 'Gagauz', 'https://flagpedia.net/data/flags/h80/md.png'), -- Автономия в Молдове
+    ('Karakalpakstan', 'Karakalpak', 'https://flagpedia.net/data/flags/h80/uz.png'), -- Автономия в Узбекистане
+    ('Afghanistan', 'Turkmen', 'https://flagpedia.net/data/flags/h80/af.png'),
+    ('Iraq', 'Iraqi Turkmen', 'https://flagpedia.net/data/flags/h80/iq.png'),
+    ('Syria', 'Syrian Turkmen', 'https://flagpedia.net/data/flags/h80/sy.png'),
+    ('Georgia', 'Meskhetian Turk', 'https://flagpedia.net/data/flags/h80/ge.png');
